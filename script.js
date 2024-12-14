bdff28f4d6c5166faf9d197f2493cca70b4deace
ahmed-shehawy <ahmedshhawy@icloud.com>
2024-12-14 14:33:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // منع الإرسال الافتراضي للنموذج
+    event.preventDefault(); 
     const name = document.querySelector('input[name="name"]').value;
-    const place = document.querySelector('input[name="place"]').value; // تغيير 'email' إلى 'place'
-    const date = document.querySelector('input[name="date"]').value; // تغيير 'message' إلى 'date'
+    const place = document.querySelector('input[name="place"]').value; 
+    const date = document.querySelector('input[name="date"]').value; 
     
-    // عرض رسالة تأكيد
+
     const responseMessage = `thank you ${name}, A request has been received ${place} in ${date},we will reply to you as soon as possible .`;
     document.getElementById('responseMessage').innerText = responseMessage;
-    document.getElementById('contactForm').reset(); // إعادة تعيين النموذج
+    document.getElementById('contactForm').reset(); 
 });
